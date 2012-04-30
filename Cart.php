7cbed5c99dcfaa8b604aca457709fc46201ddc00
Pavel Kovalyov <sxe.funky@gmail.com>
2012-04-30 13:34:35 +0300

    temporary disabled display format for Zend_Currency

--- Cart.php
+++ Cart.php
@@ -65,12 +65,12 @@ class Cart extends Tools_Cart_Cart {
 	private function _initCurrency() {
 		if (!Zend_Registry::isRegistered('Zend_Currency')){
 			$this->_currency = new Zend_Currency(array(
-				'display' => Zend_Currency::NO_SYMBOL
+//				'display' => Zend_Currency::NO_SYMBOL
 			));
 		} else {
 			$this->_currency = Zend_Registry::get('Zend_Currency');
 			$this->_currency->setFormat(array(
-				'display' => Zend_Currency::NO_SYMBOL
+//				'display' => Zend_Currency::NO_SYMBOL
 			));
 		}
 		$correctCurrency                = isset($this->_shoppingConfig['currency']) ? $this->_shoppingConfig['currency'] : self::DEFAULT_CURRENCY_NAME;
