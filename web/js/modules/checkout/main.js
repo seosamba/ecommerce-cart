e147df5ee2a769d25be81a8cf3ea4d71824f7943
Pavel Kovalyov <sxe.funky@gmail.com>
2012-09-06 19:25:13 +0300

    removed website url from libs path

--- web/js/modules/checkout/main.js
+++ web/js/modules/checkout/main.js
@@ -3,9 +3,9 @@
  */
 require.config({
     paths: {
-        'underscore': $('#website_url').val() + 'plugins/shopping/web/js/libs/underscore/underscore-min',
-        'backbone'  : $('#website_url').val() + 'plugins/shopping/web/js/libs/backbone/backbone-min',
-        'text'      : $('#website_url').val() + 'plugins/shopping/web/js/libs/require/text'
+        'underscore': '/plugins/shopping/web/js/libs/underscore/underscore-min',
+        'backbone'  : '/plugins/shopping/web/js/libs/backbone/backbone-min',
+        'text'      : '/plugins/shopping/web/js/libs/require/text'
     },
     shim: {
         underscore: {exports: '_'},
