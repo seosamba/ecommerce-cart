381869b15fc24e95ca7485e170ea41674a4de7ba
Pavel Kovalyov <sxe.funky@gmail.com>
2012-11-16 18:04:13 +0200

    fixed multiple addtocart binding

--- Cart.php
+++ Cart.php
@@ -87,6 +87,11 @@ class Cart extends Tools_Cart_Cart {
 
 	}
 
+	public function beforeController(){
+		$layout = Zend_Layout::getMvcInstance();
+		$layout->getView()->headScript()->appendFile($this->_websiteUrl.'plugins/cart/web/js/toastercart.js');
+	}
+
 	private function _initCurrency() {
 		if (!Zend_Registry::isRegistered('Zend_Currency')){
 			$this->_currency = new Zend_Currency(self::DEFAULT_LOCALE);
---
+++ web/js/toastercart.js
@@ -0,0 +1,20 @@
+/**
+ * Just to make sure that add to cart handler is attached
+ */
+$(function() {
+    $(document).on('click', 'a.tcart-add[data-pid]', function(e) {
+        e.preventDefault();
+        var pid  = $(this).data('pid');
+        var qty = 1;
+        if($('input[name="productquantity-' + pid + '"]').length > 0){
+            qty = parseInt($('input[name="productquantity-' + pid + '"]').val());
+        }
+        $.post($('#website_url').val()+'plugin/cart/run/cart/', {
+            pid     : pid,
+            options : $('div[data-productid=' + pid + '] *').serialize(),
+            qty     : qty
+        }, function() {
+            window.location.href = $('#website_url').val()+'plugin/cart/run/checkout';
+        })
+    })
+});
