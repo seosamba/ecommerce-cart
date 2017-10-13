dba983165701e69ff2d0ebd061ba5b09ba185930
Vorobyov Valentin <vorobyovvalentin@gmail.com>
2017-10-13 12:50:39 +0300

    add spain translate

--- web/js/nls/es_ES_ln.js
+++ web/js/nls/es_ES_ln.js
@@ -1,5 +1,14 @@
 define({
     "root": {
-
+        'Select this pickup location' : 'Select this pickup location',
+        'Sunday' : 'Sunday',
+        'Monday' : 'Monday',
+        'Tuesday' : 'Tuesday',
+        'Wednesday' : 'Wednesday',
+        'Thursday' : 'Thursday',
+        'Friday' : 'Friday',
+        'Saturday' : 'Saturday',
+        'Missing required fields' : 'Missing required fields',
+        'Please enter location' : 'Please enter location'
     }
 });
