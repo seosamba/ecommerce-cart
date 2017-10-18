d8ec63d0f6e50ff833473439938b0c4a60da6e89
Vorobyov Valentin <vorobyovvalentin@gmail.com>
2017-10-18 11:31:41 +0300

    Add spain translate in nls

--- web/js/nls/es_ln.js
+++ web/js/nls/es_ln.js
@@ -1,14 +1,14 @@
 define({
     "root": {
-        'Select this pickup location' : 'Select this pickup location',
-        'Sunday' : 'Sunday',
-        'Monday' : 'Monday',
-        'Tuesday' : 'Tuesday',
-        'Wednesday' : 'Wednesday',
-        'Thursday' : 'Thursday',
-        'Friday' : 'Friday',
-        'Saturday' : 'Saturday',
-        'Missing required fields' : 'Missing required fields',
-        'Please enter location' : 'Please enter location'
+        'Select this pickup location' : 'Seleccionar dirección de recogida en tienda',
+        'Sunday' : 'Domingo',
+        'Monday' : 'Lunes',
+        'Tuesday' : 'Martes',
+        'Wednesday' : 'Miercoles',
+        'Thursday' : 'Jueves',
+        'Friday' : 'Viernes',
+        'Saturday' : 'Sabado',
+        'Missing required fields' : 'Faltan campos oblogatorios por rellenar',
+        'Please enter location' : 'Por favor, introduzca una dirección'
     }
 });
